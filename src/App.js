90c15dfc230c42c83b32890be5e3299f7f583a05
Scrubi <51309968+Scrubi@users.noreply.github.com>
2020-05-15 12:55:20 +0300

    Created first React app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 import './App.css';
+import {v4 as uuid} from 'uuid';
+import peepo from './peepowide.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: uuid(),
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: uuid(),
+        title: 'Dinner with wife',
+        completed: false
+      },
+      {
+        id: uuid(),
+        title: 'Meeting with boss',
+        completed: false
+      }
+    ]
+  }
+    // Toggle Complete
+    markComplete = (id) => {
+      this.setState({ todos: this.state.todos.map(todo => {
+        if(todo.id === id){
+          todo.completed = !todo.completed
+        }
+        return todo;
+      }) });
+    }
+
+    // Delete Todo
+    delTodo = (id) => {
+      this.setState({ todos: [...this.state.todos.filter(todo => todo.id 
+        !== id)] });
+    }
+
+    // Add Todo
+    addTodo = (title) => {
+      const newTodo = {
+        id: uuid(),
+        title,
+        completed: false
+      }
+      this.setState({ todos: [...this.state.todos, newTodo] })
+    }
+
+    render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <img src={peepo}/>
+          <Header />          
+          <AddTodo addTodo={this.addTodo}/>
+          <Todos todos={this.state.todos} markComplete={this.markComplete}
+            delTodo={this.delTodo}/>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
